f03624a1561386fa2446d11fa48300f2eb60d6f1
thejaustin <the.j.austin@gmail.com>
2026-05-05 04:38:43 -0500

    feat: add remove method to ClientManager

--- server-shared/src/main/java/rikka/shizuku/server/ClientManager.java
+++ server-shared/src/main/java/rikka/shizuku/server/ClientManager.java
@@ -86,6 +86,12 @@ public class ClientManager<ConfigMgr extends ConfigManager> {
         clientRecords.add(clientRecord);
         return clientRecord;
     }
+
+    public void remove(String packageName) {
+        synchronized (this) {
+            clientRecords.removeIf(clientRecord -> clientRecord.packageName.equals(packageName));
+        }
+    }
 }
 
 
